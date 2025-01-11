090157e4828f9fd5d30f18da2764cc99f9cc17ef
akamiya208 <a.kamiya.208@gmail.com>
2025-01-11 04:22:20 +0000

    chore: add test

--- internal/app/task_handler_test.go
+++ internal/app/task_handler_test.go
@@ -25,19 +25,23 @@ func (m *MockedMySQLClient) GetTask(taskID uint) (models.Task, error) {
 }
 
 func (m *MockedMySQLClient) GetTasksByName(name string) ([]models.Task, error) {
-	return nil, nil
+	args := m.Called(name)
+	return args.Get(0).([]models.Task), args.Error(1)
 }
 
 func (m *MockedMySQLClient) CreateTask(task *models.Task) error {
-	return nil
+	args := m.Called(task)
+	return args.Error(0)
 }
 
 func (m *MockedMySQLClient) UpdateTask(task *models.Task) error {
-	return nil
+	args := m.Called(task)
+	return args.Error(0)
 }
 
 func (m *MockedMySQLClient) DeleteTask(task *models.Task) error {
-	return nil
+	args := m.Called(task)
+	return args.Error(0)
 }
 
 func (m *MockedMySQLClient) DB() *gorm.DB {
@@ -55,7 +59,7 @@ func TestHandleGetTask(t *testing.T) {
 		DeletedAt:   gorm.DeletedAt{},
 	}
 
-	t.Run("success", func(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
 		taskID := uint(1)
 		mockedMySQLClient := new(MockedMySQLClient)
 		mockedMySQLClient.On("GetTask", taskID).Return(testTask, nil)
@@ -73,5 +77,64 @@ func TestHandleGetTask(t *testing.T) {
 		var response dto.TaskResponse
 		json.NewDecoder(w.Body).Decode(&response)
 		assert.Equal(t, "task1", response.Name)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		taskID := uint(1)
+		mockedMySQLClient := new(MockedMySQLClient)
+		mockedMySQLClient.On("GetTask", taskID).Return(models.Task{}, fmt.Errorf("record not found"))
+		handlers := NewTaskHandler(mockedMySQLClient)
+
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/tasks/%d", taskID), nil)
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("GET /api/v1/tasks/{taskId}", handlers.HandleGetTask)
+		mux.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusNotFound, w.Code)
+
+		var response dto.ErrorResponse
+		json.NewDecoder(w.Body).Decode(&response)
+		assert.Equal(t, "Not Found", response.Status)
+	})
+
+	t.Run("internal server error", func(t *testing.T) {
+		taskID := uint(1)
+		mockedMySQLClient := new(MockedMySQLClient)
+		mockedMySQLClient.On("GetTask", taskID).Return(models.Task{}, fmt.Errorf("some error"))
+		handlers := NewTaskHandler(mockedMySQLClient)
+
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/tasks/%d", taskID), nil)
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("GET /api/v1/tasks/{taskId}", handlers.HandleGetTask)
+		mux.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+
+		var response dto.ErrorResponse
+		json.NewDecoder(w.Body).Decode(&response)
+		assert.Equal(t, "Internal Server Error", response.Status)
+	})
+
+	t.Run("bad request", func(t *testing.T) {
+		taskID := "invalid"
+		mockedMySQLClient := new(MockedMySQLClient)
+		handlers := NewTaskHandler(mockedMySQLClient)
+
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/api/v1/tasks/%s", taskID), nil)
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("GET /api/v1/tasks/{taskId}", handlers.HandleGetTask)
+		mux.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+
+		var response dto.ErrorResponse
+		json.NewDecoder(w.Body).Decode(&response)
+		assert.Equal(t, "Bad Request", response.Status)
 	})
 }
